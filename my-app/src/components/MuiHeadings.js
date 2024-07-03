d2e7c8d9aca818316da6125aff591007952a60aa
Alec Nghia <hieunghiabeo@gmail.com>
2024-07-03 11:35:08 +0700

    Add components folder; add classes

---
+++ my-app/src/components/MuiHeadings.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+
+export const MyHeadings = () => {
+    return (
+        <div>
+            <Typography variant='h1'>H1 Headings</Typography>
+            <Typography variant='h2'>H2 Headings</Typography>
+            <Typography variant='h3'>H3 Headings</Typography>
+            <Typography variant='h4'>H4 Headings</Typography>
+            <Typography variant='h5'>H5 Headings</Typography>
+            <Typography variant='h6'>H6 Headings</Typography>
+        </div>
+    )
+}
---
+++ my-app/src/components/MuiTextfield.js
@@ -0,0 +1,17 @@
+import { Box, InputLabel, TextField, Stack } from "@mui/material"
+
+export function MyTextfield() {
+    return (
+        <Stack
+            className="textfield"
+            component="form"
+            autoComplete="off"
+            noValidate
+            direction={"column"}
+        >
+            <TextField label="Username" variant="outlined" />
+            <TextField label="Password" variant="outlined"/>
+        </Stack>
+    );
+   
+}
