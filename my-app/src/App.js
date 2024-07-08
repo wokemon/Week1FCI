1aac5e3882e702184f6c86693643a73d35eff949
Alec Nghia <hieunghiabeo@gmail.com>
2024-07-08 14:41:26 +0700

    Add some comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import logo from './logo.svg';
 import './App.css';
 import AppNav from './components/Navbar';
@@ -9,32 +10,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <AppNav/>
+        // have to put this in here for homepage
+        <AppNav />
         <Routes>
           <Route path="/login" element={<SignIn />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
       </BrowserRouter>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <MyButton />
-          <MyHeadings />
-        </div>
-      </header> */}
-      {/* <AppNav />
-      <MyTextfield /> */}
     </div>
   );
 }
